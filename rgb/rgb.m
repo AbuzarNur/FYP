b355f098fb6fedd79f216cf93fcb47f52b138b4c
AbuzarNur <36696875+AbuzarNur@users.noreply.github.com>
2018-03-28 18:08:04 +1100

    Add files via upload

--- rgb/rgb.m
+++ rgb/rgb.m
@@ -4,26 +4,46 @@ clc;clear all;close all;
 faceDetector = vision.CascadeObjectDetector();
 
 % Read a video frame and run the face detector.
-videoFileReader = vision.VideoFileReader('rgb.mpg');
-file            = VideoReader('rgb.mpg');
-file2           = VideoReader('rgb.mpg');
+video           = 'batch.mov';
+videoFileReader = vision.VideoFileReader(video);
+file            = VideoReader(video);
 videoFrame      = step(videoFileReader);
 bbox            = step(faceDetector, videoFrame);
-bbox            = round(bbox.*[1.3,1.15,0.4,0.15]);
+
+% Finding the biggest box
+bbox_size = size(bbox);
+for i = 1:bbox_size(1)
+    area(i) = round(bbox(i,3)*bbox(i,4));
+end
+[~, j] = max(area);
+bbox = bbox(j,:);
+
+% Finding ROI
+x_scale = 0.25;
+x_centre = round(bbox(1) + bbox(3)/2);
+x_left = round(x_centre - bbox(3)*x_scale);
+x_right = round(x_centre + bbox(3)*x_scale);
+
+y_scale = 0.07;
+y_centre = round(bbox(2) + bbox(4)/2);
+y_centre = round(y_centre - bbox(4)*0.35);
+y_top = round(y_centre - bbox(3)*y_scale);
+y_bottom = round(y_centre + bbox(3)*y_scale);
+
+ROI = [x_left,y_top,(x_right-x_left),(y_bottom-y_top)];
 
 % Draw the returned bounding box around the detected face.
-videoFrame = insertShape(videoFrame, 'Rectangle', bbox);
-figure; imshow(videoFrame); title('Detected face');
+videoFrame = insertShape(videoFrame, 'Rectangle', ROI);
+figure; imshow(videoFrame); title('ROI'); hold on;
 
 % Convert the first box into a list of 4 points
 % This is needed to be able to visualize the rotation of the object.
-bboxPoints = bbox2points(bbox(1, :));
+bboxPoints = bbox2points(ROI(1,:));
 
 % Detect feature points in the face region.
-points = detectMinEigenFeatures(rgb2gray(videoFrame), 'ROI', bbox);
+points = detectMinEigenFeatures(rgb2gray(videoFrame), 'ROI', ROI,'MinQuality',0.004);
 
 % Display the detected points.
-figure, imshow(videoFrame), hold on, title('Detected features');
 plot(points);
 
 % Create a point tracker and enable the bidirectional error constraint to
@@ -35,15 +55,15 @@ pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
 points = points.Location;
 initialize(pointTracker, points, videoFrame);
 
-videoPlayer  = vision.VideoPlayer('Position',...
-    [100 100 [size(videoFrame, 2), size(videoFrame, 1)]+30]);
+% videoPlayer  = vision.VideoPlayer('Position',...
+% [100 100 [size(videoFrame, 2), size(videoFrame, 1)]+30]);
 
 % Make a copy of the points to be used for computing the geometric
 % transformation between the points in the previous and the current frames
 oldPoints = points;
-NoF = file.NumberOfFrames;
+NoF = VideoReader(video).NumberOfFrames;
 
-green_average = zeros(1,NoF);
+green_average = zeros(1, NoF);
 
 for i = 1:NoF
     % get the next frame
@@ -65,13 +85,7 @@ for i = 1:NoF
         bboxPoints = transformPointsForward(xform, bboxPoints);
                 
         % Insert a bounding box around the object being tracked
-        bboxPolygon = reshape(bboxPoints', 1, []);
-%         videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, ...
-%             'LineWidth', 2);
-                
-        % Display tracked points
-%         videoFrame = insertMarker(videoFrame, visiblePoints, '+', ...
-%             'Color', 'white');       
+        bboxPolygon = reshape(bboxPoints', 1, []);     
         
         % Reset the points
         oldPoints = visiblePoints;
@@ -81,14 +95,15 @@ for i = 1:NoF
     rows = round(bboxPolygon(1,2):bboxPolygon(1,6));
     columns = round(bboxPolygon(1,1):bboxPolygon(1,3));
     
-    frame = readFrame(file2);
-    green_average(i) = mean(mean(squeeze(frame(rows,columns,2))));
-    
-    % Display the annotated video frame using the video player object
-    step(videoPlayer, videoFrame);
+    frame = readFrame(file);
+    green = squeeze(frame(rows,columns,2));
+    green_average(i) = mean(mean(green));
 end
 
 % Clean up
 release(videoFileReader);
-release(videoPlayer);
+% release(videoPlayer);
 release(pointTracker);
+
+figure; 
+plot(1:NoF,green_average);
